02ab6959f649c2fe812f8e3298bc1148c4d01139
Rajiv <rajiv@Mac.attlocal.net>
2025-07-29 15:25:58 -0400

    created project 7 files

---
+++ Project7_Crewmates/src/components/Card.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './Card.css'
+import more from './more.png'
+import { Link } from 'react-router-dom'
+import {supabase} from '../client'
+
+
+const Card = (props) =>  {
+
+  const [count, setCount] = useState(props.betCount)
+
+  const updateCount = async(event) => {
+    event.preventDefault()
+    const {data} = await supabase.from('Posts').update({betCount : count + 1}).eq('id', id)
+    setCount(data)
+  }
+
+  return (
+      <div className="Card">
+          <Link to={'edit/'+ props.id}><img className="moreButton" alt="edit button" src={more} /></Link>
+          <h2 className="title">{props.title}</h2>
+          <h3 className="author">{"by " + props.author}</h3>
+          <p className="description">{props.description}</p>
+          <button className="betButton" onClick={updateCount} >👍 Bet Count: {count}</button>
+      </div>
+  );
+};
+
+export default Card
