06c0bbb282598dc448f6d5b6577644e1c42f077a
Rajiv <rajiv@Mac.attlocal.net>
2025-08-02 14:09:28 -0400

    Added the client .js files for previous projects

--- Project7_Crewmates/src/client.js
+++ Project7_Crewmates/src/client.js
@@ -1,8 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-
-const URL = 'https://igzqkdlfdqpjcqcugrle.supabase.co'
-const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlnenFrZGxmZHFwamNxY3VncmxlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzMTU5MTQsImV4cCI6MjA2ODg5MTkxNH0.ApopSMxURkVDLiYMEamCDQw3tb78t2yGMngPB6KjvHQ'
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(URL, API_KEY)
-
+export const supabase = createClient(supabaseUrl, supabaseKey)
